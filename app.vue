bf02538ef970996dcd0e70ef5acc5d41b8f5cbf4
kou-yoshida <kouyoshida28@gmail.com>
2023-11-29 22:24:57 +0900

    サブモジュールを追加

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtWelcome />    
   </div>
 </template>
+<script setup lang="ts">
+/**
+ * 1 submoduleの変更を探知する
+ * 2 submoduleのdoc.ymlを、こちらのdoc.ymlにコピーする
+ * 3 任意のコマンドを実行する
+ * 4 コミットしてPRを作成するまで
+ */
+</script>
